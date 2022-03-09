579e2d1bb40bb58c7576b77bc6353e89da568dc0
nelsueroo <101257330+nelsueroo@users.noreply.github.com>
2022-03-09 13:31:43 +0100

    Inici Projecte Nel
    Ha començat la nova era.

---
+++ Sprite.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(500,300)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _process(delta):
+	position += Vector2(3,0)
