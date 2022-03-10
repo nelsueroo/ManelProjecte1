237be0b4c8bf6aa7b7e983a58163b8740efe58e0
nelsueroo <101257330+nelsueroo@users.noreply.github.com>
2022-03-10 09:00:40 +0100

    Primera classe de Godot
    Hem après a rotar i a que funcionin sols.

--- Sprite.gd
+++ Sprite.gd
@@ -4,6 +4,8 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export var velocitat = Vector2(200,300)
+#var velocitat = Vector2(200,0) #Vector2(200,0) la podem definir com una variable
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,4 +17,11 @@ func _ready():
 #func _process(delta):
 #	pass
 func _process(delta):
-	position += Vector2(3,0)
+	position += velocitat * delta #delta son els segons que han passat
+	rotation_degrees += 30 * delta
+
+	if position.x >= 1000 or position.x <= 0:
+		velocitat.x = -velocitat.x
+	if position.y >= 600 or position.y <= 0:
+		velocitat.y = -velocitat.y
+		
