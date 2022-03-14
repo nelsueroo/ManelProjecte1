60c09d670ee0be6b2f9fe421bbd236b9126d7fcc
nelsueroo <101257330+nelsueroo@users.noreply.github.com>
2022-03-14 14:29:23 +0100

    Amunt avall dreta esquerra

--- Sprite.gd
+++ Sprite.gd
@@ -1,10 +1,12 @@
-extends Sprite
+extends Area2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var velocitat = Vector2(200,300)
+export var velocitat = 400
+var velocitat_angular = 30
+var direccio = Vector2(0,0)
 #var velocitat = Vector2(200,0) #Vector2(200,0) la podem definir com una variable
 
 
@@ -17,11 +19,23 @@ func _ready():
 #func _process(delta):
 #	pass
 func _process(delta):
-	position += velocitat * delta #delta son els segons que han passat
-	rotation_degrees += 30 * delta
-
-	if position.x >= 1000 or position.x <= 0:
-		velocitat.x = -velocitat.x
-	if position.y >= 600 or position.y <= 0:
-		velocitat.y = -velocitat.y
+	direccio = Vector2.ZERO
+	if Input.is_action_pressed("Adalt"):
+		direccio += Vector2(0,-1)
+	if Input.is_action_pressed("Abaix"):
+		direccio += Vector2(0,1)
+	if Input.is_action_pressed("Dreta"):
+		direccio += Vector2(1,0)
+	if Input.is_action_pressed("Esquerra"):
+		direccio += Vector2(-1,0)
+		
+	position += direccio.normalized() * velocitat * delta 
+	#position += velocitat * delta #delta son els segons que han passat
+	#rotation_degrees += 30 * delta
+	#$Segon.rotation_degrees += velocitat_angular
+	
+	#if position.x >= 1000 or position.x <= 0:
+		#velocitat.x = -velocitat.x
+	#if position.y >= 600 or position.y <= 0:
+		#velocitat.y = -velocitat.y
 		
